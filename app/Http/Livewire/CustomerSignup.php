67d2e1dc07a47be33f1d1b96ec851731b2a7c813
Fahim-kh <111640774+Muaaz8@users.noreply.github.com>
2023-07-08 21:13:35 +0500

    multiselect

--- app/Http/Livewire/CustomerSignup.php
+++ app/Http/Livewire/CustomerSignup.php
@@ -30,7 +30,8 @@ class CustomerSignup extends Component
     public $custom_states = [];
     public $custom_banks = [];
     public $custom_bank_select = '';
-
+    public $selectedbanks = [];
+    public $selected = [];
     protected $rules = [
         'bank_name' => 'required',
         'bank_email' => 'required',
@@ -46,6 +47,9 @@ class CustomerSignup extends Component
         'admin_gender' => 'required',
         'subscription' => 'required',
     ];
+    protected $listeners = [
+        'selected'
+     ];
 
     public function render()
     {
@@ -56,8 +60,8 @@ class CustomerSignup extends Component
 
     public function submitForm()
     {
-        $this->validate();
-        $bank = CB::create([
+        if($this->custom_banks != []){
+            $bank = CB::create([
             'bank_name' => $this->bank_name,
             'bank_email' => $this->bank_email,
             'bank_phone_numebr' => $this->bank_phone,
@@ -65,36 +69,39 @@ class CustomerSignup extends Component
             'msa_code' => $this->bank_msa,
             'state' => $this->bank_state,
             'display_reports' => $this->subscription,
-        ]);
-        $user = User::create([
-            'name' => $this->admin_name,
-            'email' => $this->admin_email,
-            'phone_number' => $this->admin_phone,
-            'designation' => $this->admin_designation,
-            'employee_id' => $this->admin_employeeid,
-            'gender' => $this->admin_gender,
-            'password' => bcrypt($this->admin_phone),
-            'bank_id' => $bank->id,
-        ]);
-        if($this->subscription == 'custom'){
-            foreach($this->custom_banks as $key => $custom_bank) {
-                $custom_selected_banks = CustomPackageBanks::create([
-                    'bank_id' => $bank->id,
-                    'customer_selected_bank_id' => $custom_bank,
-                ]);
+            ]);
+            $user = User::create([
+                'name' => $this->admin_name,
+                'email' => $this->admin_email,
+                'phone_number' => $this->admin_phone,
+                'designation' => $this->admin_designation,
+                'employee_id' => $this->admin_employeeid,
+                'gender' => $this->admin_gender,
+                'password' => bcrypt($this->admin_phone),
+                'bank_id' => $bank->id,
+            ]);
+            if($this->subscription == 'custom'){
+                foreach($this->custom_banks as $key => $custom_bank) {
+                    $custom_selected_banks = CustomPackageBanks::create([
+                        'bank_id' => $bank->id,
+                        'customer_selected_bank_id' => $custom_bank,
+                    ]);
+                }
             }
+            $charges = Packages::where('package_type',$this->subscription)->first();
+            $contract = Contract::create([
+                'contract_start' => date('Y-m-d'),
+                'contract_end' => date('Y-m-d', strtotime(date('Y-m-d'). ' + 1 year')),
+                'charges' => $charges->price,
+                'bank_id' => $bank->id,
+            ]);
+            $role = Role::where('slug', 'bank-admin')->first();
+            $user->roles()->attach($role);
+            $this->clear();
+            return redirect(url('/signin'));
+        }else{
+            $this->addError('customer_banks', 'You have to Select atleast 1 bank');
         }
-        $charges = Packages::where('package_type',$this->subscription)->first();
-        $contract = Contract::create([
-            'contract_start' => date('Y-m-d'),
-            'contract_end' => date('Y-m-d', strtotime(date('Y-m-d'). ' + 1 year')),
-            'charges' => $charges->price,
-            'bank_id' => $bank->id,
-        ]);
-        $role = Role::where('slug', 'bank-admin')->first();
-        $user->roles()->attach($role);
-        $this->clear();
-        return redirect(url('/signin'));
         $this->render();
     }
 
@@ -118,5 +125,32 @@ class CustomerSignup extends Component
         $this->admin_employeeid = '';
         $this->admin_gender = '';
         $this->subscription = '';
+    }
+    public function selected($value)
+    {
+        $this->custom_states = $value;
+        $this->selectlist();
+    }
+
+    public function addArray($item){
+        if($item != ""){
+            if(!in_array($item,$this->selected)){
+                array_push($this->selected,$item);
+            }else{
+                unset($this->selected[array_search($item,$this->selected)]);
+            }
+        }
+        $this->custom_states = $this->selected;
+    }
+
+    public function addBanks($item){
+        if($item != ""){
+            if(!in_array($item,$this->selectedbanks)){
+                array_push($this->selectedbanks,$item);
+            }else{
+                unset($this->selectedbanks[array_search($item,$this->selectedbanks)]);
+            }
+        }
+        $this->custom_banks = $this->selectedbanks;
     }
 }
--- resources/views/livewire/customer-signup.blade.php
+++ resources/views/livewire/customer-signup.blade.php
@@ -22,7 +22,7 @@
                                                 <div class="mb-3">
                                                     <label for="bank_email" class="form-label">Email</label>
                                                     <input type="email" id="bank_email" name="bank_email" class="form-control" aria-describedby="email" wire:model.lazy="bank_email" required>
-                                    
+
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
@@ -35,14 +35,14 @@
                                                 <div class="mb-3">
                                                     <label for="bank_website" class="form-label">Website</label>
                                                     <input type="text" id="bank_website" name="bank_website" class="form-control" aria-describedby="website" wire:model.lazy="bank_website" required>
-                                    
+
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
                                                 <div class="mb-3">
                                                     <label for="bank_msa" class="form-label">MSA Code</label>
                                                     <input type="text" id="bank_msa" name="bank_msa" class="form-control" aria-describedby="msa_code" wire:model.lazy="bank_msa" required>
-                                    
+
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
@@ -54,7 +54,7 @@
                                                         <option value="{{$state->id}}">{{$state->name}}</option>
                                                         @endforeach
                                                     </select>
-                                    
+
                                                 </div>
                                             </div>
                                         </div>
@@ -77,28 +77,28 @@
                                                 <div class="mb-3">
                                                     <label for="admin_email" class="form-label">Email</label>
                                                     <input type="email" id="admin_email" name="admin_email" class="form-control" aria-describedby="email" wire:model.lazy="admin_email" required>
-                                    
+
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
                                                 <div class="mb-3">
                                                     <label for="admin_phone" class="form-label">Phone Number</label>
                                                     <input type="number" id="admin_phone" name="admin_phone" class="form-control" aria-describedby="phone" wire:model.lazy="admin_phone" required>
-                                    
+
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
                                                 <div class="mb-3">
                                                     <label for="admin_designation" class="form-label">Designation</label>
                                                     <input type="text" id="admin_designation" name="admin_designation" class="form-control" aria-describedby="Designation" wire:model.lazy="admin_designation" required>
-                                    
+
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
                                                 <div class="mb-3">
                                                     <label for="admin_id" class="form-label">Employee Id</label>
                                                     <input type="text" id="admin_id" name="admin_employeeid" class="form-control" aria-describedby="msa_code" wire:model.lazy="admin_employeeid" required>
-                                    
+
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
@@ -109,7 +109,7 @@
                                                         <option value="male">Male</option>
                                                         <option value="female">Female</option>
                                                     </select>
-                                    
+
                                                 </div>
                                             </div>
                                         </div>
@@ -138,33 +138,59 @@
                                             <div class="col-md-6">
                                                 <div class="mb-3">
                                                     <label for="custom_states" class="form-label">State</label>
-                                                    <select class="form-select" id="custom_states" name="custom_states[]" aria-label="Default select example" wire:model.lazy="custom_states" multiple>
-                                                        <!-- <option value="">Select State</option> -->
-                                                        @foreach($states as $state)
-                                                        <option value="{{$state->id}}">{{$state->name}}</option>
+                                                    <div class="d-flex">
+                                                        @foreach ($selected as $key => $item)
+                                                            <p class="border border-secondary rounded"> {{ $item }}
+                                                                <button type="button" class="btn" wire:click="delete({{ $key }})"><span
+                                                                        class="fa fa-close"></span></button>
+                                                            </p> &nbsp;&nbsp;
                                                         @endforeach
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-3">
-                                                <div class="mt-5 text-center">
-                                                    <button wire:click.prevent="selectlist()" class="btn submit_btn">Show Banks</button>
+                                                    </div>
+                                                    <div>
+                                                        <select class="form-control" wire:change="addArray($event.target.value)">
+                                                            <option value="">Select Option</option>
+                                                            @foreach ($states as $item)
+                                                                <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                                            @endforeach
+                                                        </select>
+                                                        <div class="mt-5 text-center">
+                                                            <button wire:click.prevent="selectlist()" class="btn submit_btn">Show Banks</button>
+                                                        </div>
+                                                    </div>
+                                                    {{-- @livewire('select2-dropdown',['data'=>$states]) --}}
                                                 </div>
                                             </div>
                                             @endif
+                                            <div class="col-md-6">
+                                            </div>
                                             @if($this->custom_bank_select != '' && $this->subscription == 'custom')
                                             <div class="col-md-6">
                                                 <div class="mb-3">
                                                     <label for="custom_banks" class="form-label">Banks</label>
-                                                    <select class="form-select" id="custom_banks" name="custom_banks[]" aria-label="Default select example" wire:model.lazy="custom_banks" multiple>
-                                                        <!-- <option value="">Select Banks</option> -->
-                                                        @foreach($this->custom_bank_select as $bank)
-                                                        <option value="{{$bank->id}}">{{$bank->name}}</option>
+                                                    <div class="d-flex">
+                                                        @foreach ($selectedbanks as $key => $item)
+                                                            <p class="border border-secondary rounded"> {{ $item }}
+                                                                <button type="button" class="btn" wire:click="delete({{ $key }})"><span
+                                                                        class="fa fa-close"></span></button>
+                                                            </p> &nbsp;&nbsp;
                                                         @endforeach
-                                                    </select>
+                                                    </div>
+                                                    <div>
+                                                        <select class="form-control" wire:change="addBanks($event.target.value)">
+                                                            <option value="">Select Option</option>
+                                                            @foreach ($this->custom_bank_select as $item)
+                                                                <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
                                                 </div>
                                             </div>
                                             @endif
+                                            @error('customer_banks')
+                                                <div class="mt-3 text-center mb-5">
+                                                    <span class="alert alert-danger" role="alert">{{ $message }}</span>
+                                                </div>
+                                            @enderror
                                             <div class="col-md-12">
                                                 <div class="mb-3 text-center">
                                                     <button type="submit" class="btn submit_btn">Submit</button>
